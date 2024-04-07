43f7538789119788a8d3b3ac3e02f9c497275b18
minitabbattery <rahulpai@google.com>
2024-04-06 20:59:36 -0700

    Remove all files

--- list_files.php
+++
@@ -1,5 +0,0 @@
-<?php
-$files = array_values(array_filter(scandir('.'), function($file) {
-    return pathinfo($file, PATHINFO_EXTENSION) === 'txt';
-}));
-echo json_encode($files);
--- load_files.php
+++
@@ -1,4 +0,0 @@
-<?php
-$file = $_GET['file'];
-$contents = file_get_contents($file);
-echo json_encode($contents);
